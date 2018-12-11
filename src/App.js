49d5e9205e10a7c54f0edae9eed95a234e64df66
Leo <lcskode@gmail.com>
2018-12-11 15:31:06 +0800

    Added some comments.

--- src/App.js
+++ src/App.js
@@ -15,11 +15,13 @@ class App extends Component {
     showPersons: false
   }
   
+  // needs id to know what data to target for update
   changeNameHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
     })
 
+    // create new object and changes
     const person = {
       ...this.state.persons[personIndex]
     }
@@ -29,6 +31,7 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
+    // update persons info
     this.setState({persons: persons});
   }
 
@@ -36,12 +39,12 @@ class App extends Component {
     // get all persons from default state and create a new copy
     // const persons = this.state.persons.slice();
 
-    // Use spread operatots instead to create a copy of persons array inside state
+    // get all persons from state and create a copy of new array
+    // Use spread operatots instead of slice() method
     const persons = [...this.state.persons];
-
     // delete one person info when p element is clicked
     persons.splice(personIndex, 1);
-    // then save all remaining persons info
+    // then save all remaining new persons info to persons
     this.setState({persons: persons});
   }
 
@@ -71,11 +74,12 @@ class App extends Component {
           {/* Accessing persons data from state default values by using map */}
           {this.state.persons.map((person, index) => {
             return <Person
-              // key will help React update and render list efficiently
+              // key will help React update and render list efficiently, it allows React to track individual elements to find out which elements changed. This will allow React to re-render elements that has changed and not the whole lists.
               key={person.id}
               name={person.name}
               age={person.age}
               click={this.deletePersonHandler.bind(this, index)}
+              // 
               changed={(event) => this.changeNameHandler(event, person.id)}
             />
           })}
